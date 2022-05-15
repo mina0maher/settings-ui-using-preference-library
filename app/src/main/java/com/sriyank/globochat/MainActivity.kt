a762e60e9125db53fa705ecd181ab7038bebd0e0
mina0maher <86135594+mina0maher@users.noreply.github.com>
2022-05-16 00:14:46 +0200

    deal with shared preference changes

--- app/src/main/java/com/sriyank/globochat/MainActivity.kt
+++ app/src/main/java/com/sriyank/globochat/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.sriyank.globochat
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -19,16 +23,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Get NavHost and NavController
-        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController = navHostFrag.navController
         // Get AppBarConfiguration
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
         // Link ActionBar with NavController
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        //step 1:Get reference to the SharedPreferences (XML File)
+        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(this)
+        //step 2:Get the 'value' using the 'key'
+        val autoReplyTime = sharedPreference.getString(getString(R.string.key_auto_reply_time), "")
+        Log.i("mainActivity", "Auto Reply Time : $autoReplyTime")
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //called only after the preference value has changed
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+        if (p1 == getString(R.string.key_status)) {
+            val newStatus = p0?.getString(p1, "")
+            Toast.makeText(this, "new status : $newStatus", Toast.LENGTH_SHORT).show()
+        }
+        if (p1 == getString(R.string.key_auto_reply)) {
+            val autoReply = p0?.getBoolean(p1, false)
+            if (autoReply!!) {
+                Toast.makeText(this, "Auto reply: ON", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auto reply : OFF", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
     }
 }
--- app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
+++ app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
@@ -1,23 +1,29 @@
 package com.sriyank.globochat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.settings,rootKey)
+        setPreferencesFromResource(R.xml.settings, rootKey)
 
         val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
         accSettingsPref?.setOnPreferenceClickListener {
-            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+
+            val navHostFragment =
+                activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
             val navController = navHostFragment.navController
             val action = SettingsFragmentDirections.actionSettingsToAccSettings()
             navController.navigate(action)
@@ -25,6 +31,30 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
             true
         }
+        //read preference values in a fragment
+        //step 1:Get reference to the SharedPreferences (XML File)
+        val sharedPreference = PreferenceManager.getDefaultSharedPreferences(context)
+        //step 2:Get the 'value' using the 'key'
+        val autoReplyTime = sharedPreference.getString(getString(R.string.key_auto_reply_time), "")
+        Log.i("SettingsFragment", "Auto Reply Time : $autoReplyTime")
+
+        val autoDownload = sharedPreference.getBoolean(getString(R.string.key_auto_download), false)
+        Log.i("SettingsFragment", "Auto Reply Time : $autoDownload")
+
+        val statusPref = findPreference<EditTextPreference>(getString(R.string.key_status))
+        statusPref?.setOnPreferenceChangeListener { preference, newValue ->
+            Log.i("SettingsFragment", "New Status : $newValue")
+            val newStatus = newValue as String
+            if(newStatus.contains("bad")){
+                Toast.makeText(context,"inappropriate status. please maintain community guidelines.",Toast.LENGTH_SHORT).show()
+                false
+            }else{
+                true
+            }
+             //true accept new values // false reject the new value.
+        }
+
     }
+
 
 }
